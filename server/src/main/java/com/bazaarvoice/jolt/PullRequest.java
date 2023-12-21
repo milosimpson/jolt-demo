b185234fba64882351d09fc38712229350dd6fef
ranjith.raj <ranjith.raj@freshworks.com>
2023-12-21 17:17:49 +0530

    input and spec write

--- server/src/main/java/com/bazaarvoice/jolt/PullRequest.java
+++ server/src/main/java/com/bazaarvoice/jolt/PullRequest.java
@@ -1,8 +1,8 @@
 package com.bazaarvoice.jolt;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,9 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 
 import com.bazaarvoice.jolt.helper.GitConstants;
@@ -26,8 +24,9 @@ import com.bazaarvoice.jolt.helper.GitService;
 @WebServlet("/pullrequest")
 public class PullRequest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	//Update the path of jolt-specs / activity-serve local git location 
-	private static final String BASE_REPO_PATH= "<jolt_spec>/.git";
+	//Update the path of jolt-specs / activity-serve local git location
+	private static final String BASE_REPO_PATH = "<jolt-specs_location>";
+	private static final String GIT_BASE_REPO_PATH= BASE_REPO_PATH+ "/.git";
 	private Repository repository;
 
 	private GitService gitService;
@@ -38,7 +37,7 @@ public class PullRequest extends HttpServlet {
 	public PullRequest(){
 		super();
 		//TODO : Move to singleton
-		this.gitService = new GitService(BASE_REPO_PATH);
+		this.gitService = new GitService(GIT_BASE_REPO_PATH);
 		try {
 			this.repository = gitService.getHostedRepository("");
 		} catch (IOException e) {			
@@ -51,11 +50,14 @@ public class PullRequest extends HttpServlet {
 	 *      response)
 	 */
 	protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
-		String inputString, specString;
+		String inputString, specString,modelName,branchName;
 		try {
 			inputString = req.getParameter("input");
 
 			specString = req.getParameter("spec");
+			modelName = req.getParameter("modelName");
+
+			branchName = req.getParameter("branchName");
 
 		} catch (Exception e) {
 			response.getWriter().println("Could not url-decode the inputs.\n");
@@ -77,8 +79,13 @@ public class PullRequest extends HttpServlet {
 			return;
 		}
 
-		String branchName = this.raisePR("Test", GitConstants.GIT_REPOSITORY_NAME);
-		System.out.println(branchName);
+		
+		String result = this.raisePR(branchName, 
+				GitConstants.GIT_REPOSITORY_NAME,
+				JsonUtils.toPrettyJsonString(input),JsonUtils.toPrettyJsonString(spec),modelName
+				);		
+		
+		System.out.println(result);
 		String res = branchName;
 		// Add PR details in the response
 		response.getWriter().println(res);
@@ -90,7 +97,7 @@ public class PullRequest extends HttpServlet {
 		return this.gitService.getHostedRepository("");
 	}
 
-	protected String raisePR(String branchName, String repoName) {
+	protected String raisePR(String branchName, String repoName,String input, String spec, String modelName) {
 		//
 		try {
 			Repository r = findRepositoryByName(repoName);
@@ -103,15 +110,33 @@ public class PullRequest extends HttpServlet {
 			command.setName(branchName);
 			command.call();
 			r.close();
-			return branchName;
+			String inputFileName = modelName + "_input.json";	
+			String specFileName = modelName + "_spec.json";
+			String filePath = BASE_REPO_PATH + "/" + "specs/";
+			fileWrite(filePath, inputFileName, input);
+			fileWrite(filePath, specFileName, input);
+			
+			return branchName +" created successful";
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		} catch (GitAPIException e) {
 			throw new RuntimeException(e);
 		} catch (URISyntaxException e) {
 			throw new RuntimeException(e);
-		}
+		}	
+	}
 	
+	private boolean fileWrite(String path, String fileName, String content) {
+		try {
+		      FileWriter myWriter = new FileWriter(path+"/"+fileName);
+		      myWriter.write(content);
+		      myWriter.close();
+		      System.out.println("Successfully wrote to the file."+fileName);
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		return false;
 	}
 
 }
